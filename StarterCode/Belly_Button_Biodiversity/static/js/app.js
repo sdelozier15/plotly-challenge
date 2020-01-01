8fb91201a2ac96b991691da495b3a897a626e896
sdelozier15 <52932991+sdelozier15@users.noreply.github.com>
2020-01-01 14:48:17 -0600

    working hover on plots

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,36 +1,57 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    const url = "/metadata/" + sample;
+    let selector = d3.select("#sample-metadata");
+    selector.html("");
+    d3.json(url).then((metadata) => {
+      
+      Object.entries(metadata).forEach(([key, value]) => {
+        selector.append("h6")
+          .text(key + ": " + value);
+      });
+    });
 }
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  const url = "/samples/" + sample;
+  d3.json(url).then((samples) => {
+    let otu_ids = samples["otu_ids"];
+    let otu_labels = samples["otu_labels"];
+    let sample_values = samples["sample_values"];
 
-    // @TODO: Build a Bubble Chart using the sample data
+    let plot1 = {
+      labels: otu_ids.slice(0,10),
+      values: sample_values.slice(0,10),
+      hovertext: otu_labels.slice(0,10),
+      type: "pie"
+    };
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+    let plot2 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        opacity: 0.5,
+        color: otu_ids,
+        colorscale: "Portland"
+      }
+    };
+
+    let pie_data = [plot1]
+    Plotly.newPlot("pie", pie_data);
+    
+    let bubble_data = [plot2]
+    Plotly.newPlot("bubble", bubble_data);
+  });
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
+  
   var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
@@ -38,8 +59,7 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
-    // Use the first sample from the list to build the initial plots
+    
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
@@ -47,10 +67,8 @@ function init() {
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
 
-// Initialize the dashboard
 init();
